ee1816fdc25ccf5f95eed76ac50bdc626dfbb507
gxnny19 <wkdrudrjs010@gmail.com>
2025-10-27 17:31:19 +0900

    add 값검증

--- src/main/java/com/jgg/ex25_branch/Ex25BranchApplication.java
+++ src/main/java/com/jgg/ex25_branch/Ex25BranchApplication.java
@@ -1,13 +1,14 @@
 package com.jgg.ex25_branch;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.jgg.ex25_branch.mapper") 
 public class Ex25BranchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ex25BranchApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Ex25BranchApplication.class, args);
+    }
 }
