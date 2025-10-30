76495e5b281129ff50ca410bda80184c1a0bb0c7
장경건 <gxnny19@github.com>
2025-10-30 16:43:07 +0900

    add TDD

--- src/main/java/com/jgg/ex25_branch/controller/StudentController.java
+++ src/main/java/com/jgg/ex25_branch/controller/StudentController.java
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/students")
 @RequiredArgsConstructor
 public class StudentController {
-	private final StudentService studentService = new StudentService();
+	private final StudentService studentService;
 
 //	list화면: 전체 학생 목록
 	@GetMapping
---
+++ src/test/java/com/jgg/ex25_branch/mapper/StudentMapperTest.java
@@ -0,0 +1,24 @@
+package com.jgg.ex25_branch.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import com.jgg.ex25_branch.domain.StudentTest;
+
+@Mapper
+public interface StudentMapperTest {
+
+	@Insert("INSERT INTO student(NAME, email, age) VALUES \r\n" + "(#{name}, #{email}, #{age})")
+	@Options(useGeneratedKeys = true, keyProperty = "id")
+	int insert(StudentTest student);
+
+	@Select("SELECT * FROM student ORDER BY #{id} DESC")
+	List<StudentTest> findall();
+
+	@Select("SELECT * FROM student WHERE id = #{id}")
+	StudentTest findById(Long id);
+}
